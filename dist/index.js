973399bcbe536ba39a826bdf560cdf35007cbdd6
John Day <john.day@covetrus.com>
2020-12-24 11:34:42 -0600

    change to use push

--- dist/index.js
+++ dist/index.js
@@ -9830,38 +9830,42 @@ async function run() {
     var toEmail = core.getInput('toMailAddress');
     var subject = core.getInput('subject');
     var verbose = core.getInput('verbose');
-    var labelsToMonitor = core.getInput('labelsToMonitor').split(",");
+    //var labelsToMonitor = core.getInput('labelsToMonitor').split(",");
     var subjectPrefix = core.getInput('subjectPrefix');
 
     // check to make sure we match any of the labels first
     var context = github.context;
-    var issue = context.payload.issue;
-    var issueLabels = issue.labels;
+    console.log(context);
+    //var issue = context.payload.issue;
+    //var issueLabels = issue.labels;
     if (verbose) 
     {
-      console.log(issue);
+      //console.log(issue);
       console.log('TO:' + toEmail);
       console.log('FROM:' + fromEmail);
       console.log('SUBJECT:' + subject);
       console.log('SUBJECT PREFIX:' + subjectPrefix);
-      console.log('LABELS TO MONITOR:' + labelsToMonitor);
-      console.log('LABELS:' + issueLabels);
+      //console.log('LABELS TO MONITOR:' + labelsToMonitor);
+      //console.log('LABELS:' + issueLabels);
     }
 
-    // check to see if we found a label
-    shouldNotify = issueLabels.map(i => i.name).some(item => labelsToMonitor.includes(item));
+    //shouldNotify = issueLabels.map(i => i.name).some(item => labelsToMonitor.includes(item));
+    shouldNotify = true;
     if (verbose) console.log('SHOULD NOTIFY: ' + shouldNotify);
 
-    // if we found a match, continue, otherwise we are done
     if (shouldNotify) {
       var md = new Remarkable({html:true});
-      var posted_date = moment(issue.created_at).format("dddd, MMMM Do YYYY, h:mm:ss a");
-      var issueBodyPlain = 'Posted at ' + posted_date + '\nAnnouncement URL: ' + issue.html_url + '\n\n' + issue.body;
-      var issueBodyHtml = 'Posted at ' + posted_date + '<br/>Announcement URL: <a href=' + issue.html_url + '>' + issue.html_url + '</a><br/><br/>' + md.render(issue.body);
+      //var posted_date = moment(issue.created_at).format("dddd, MMMM Do YYYY, h:mm:ss a");
+      //var issueBodyPlain = 'Posted at ' + posted_date + '\nAnnouncement URL: ' + issue.html_url + '\n\n' + issue.body;
+      //var issueBodyHtml = 'Posted at ' + posted_date + '<br/>Announcement URL: <a href=' + issue.html_url + '>' + issue.html_url + '</a><br/><br/>' + md.render(issue.body);
 
+      var issueBodyPlain = 'my body plain';
+      var issueBodyHtml = 'my body html';
+      var title = 'my title';
+  
       // construct the right subject line
       if (!subjectPrefix.startsWith('__NONCE__')) {
-        subject = subjectPrefix + ' ' + issue.title;
+        subject = subjectPrefix + ' ' + title;
       }
 
       if (verbose) {
