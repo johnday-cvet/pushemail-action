6d8ff08644c4b5a6e6a38cf3573b6edbad4f9d5b
John Day <john.day@covetrus.com>
2020-12-24 19:30:19 -0600

    update dependencies

--- index.test.js
+++ index.test.js
@@ -4,7 +4,7 @@ const cp = require('child_process');
 const path = require('path');
 
 test('throws invalid number', async() => {
-    await expect(wait('foo')).rejects.toThrow('milleseconds not a number');
+    await expect(wait('foo')).rejects.toThrow('milliseconds not a number');
 });
 
 test('wait 500 ms', async() => {
